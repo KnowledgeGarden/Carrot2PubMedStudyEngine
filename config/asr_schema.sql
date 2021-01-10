13333431515aaebde0ac0980f5ee1364faf11b27
Jack Park <jackpark@gmail.com>
2021-01-09 16:59:57 -0800

    Added correct schema

--- config/asr_schema.sql
+++ config/asr_schema.sql
@@ -11,6 +11,17 @@ BEGIN
 END
 $do$;
 
+-- Switch to the tq_admin user to create the database for TQ objects.
+SET ROLE tq_admin;
+
+-- Create the database. *Custom name the database*
+CREATE DATABASE newdoctest ENCODING UTF8;
+
+-- Switch to tq_database.
+\c newdoctest
+
+SET ROLE tq_admin;
+
 --
 -- Create a schema to hide the proxy tables from public view.
 --
@@ -33,9 +44,17 @@ GRANT ALL PRIVILEGES ON tqos_asr.sentences TO tq_proxy;
 CREATE TABLE IF NOT EXISTS 
 tqos_asr.documents (
 	id text NOT NULL PRIMARY KEY,
+	label text NOT NULL,
+	url	text NOT NULL,
+	pmid text NOT NULL,
+	pmcid text NOT NULL,
 	document text NOT NULL
 );
 CREATE INDEX IF NOT EXISTS idx_doc_id ON tqos_asr.documents (id);
+CREATE INDEX IF NOT EXISTS idx_doc_label ON tqos_asr.documents (label);
+CREATE INDEX IF NOT EXISTS idx_doc_url ON tqos_asr.documents (url);
+CREATE INDEX IF NOT EXISTS idx_doc_pmid ON tqos_asr.documents (pmid);
+CREATE INDEX IF NOT EXISTS idx_doc_pmcid ON tqos_asr.documents (pmcid);
 
 GRANT ALL PRIVILEGES ON tqos_asr.documents TO tq_proxy;
 
@@ -45,5 +64,12 @@ tqos_asr.tuples (
 	tuple text NOT NULL
 );
 CREATE INDEX IF NOT EXISTS idx_tup_id ON tqos_asr.tuples (id);
-
 GRANT ALL PRIVILEGES ON tqos_asr.tuples TO tq_proxy;
+
+CREATE TABLE IF NOT EXISTS 
+tqos_asr.paragraphs (
+	id text NOT NULL PRIMARY KEY,
+	paragraph text NOT NULL
+);
+CREATE INDEX IF NOT EXISTS idx_par_id ON tqos_asr.paragraphs (id);
+GRANT ALL PRIVILEGES ON tqos_asr.paragraphs TO tq_proxy;
